7b407bd4a50a6b861bf1b382c4444e61aada0e24
LexaTheCreativity <131150634+LexaTheCreativity@users.noreply.github.com>
2026-03-02 10:11:42 +0100

    inital commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # DDoS-DoS-attack-simulation
-Project in 5G and IoT advanced course. Building a simulation of an DDoS/DoS attack with two raspberry pi using a client server architecture. 
+Project in 5G and IoT advanced course. Building a simulation of an DDoS/DoS attack with two raspberry pi using a client server architecture.
+
+## Create a New Feature or Make Other Changes:
+
+- Create a new branch
+- Make changes and commit
+- Push the branch to origin
+- Create a pull request
+
+
+## How to start
+
+1. Run server in Windows PowerShell terminal
+2. When the server is up, start running the clinet code (raspberry pi)
+3. The output should be visble in the terminal
+
+## Command to run the server
+
+```bash
+python server_PC.py
+```
---
+++ server_PC.py
@@ -0,0 +1,70 @@
+##PC1 as server
+
+import socket
+import json
+from datetime import datetime
+from rich.console import Console
+from rich.table import Table
+
+
+console = Console()
+
+table = Table(title="Incoming Packets", border_style="white")
+table.add_column("ID", style="dim", width=4)
+table.add_column("Content", style="white", width=25)
+table.add_column("Timestamp Sent", style="yellow", width=26)
+table.add_column("Timestamp Received", style="green", width=26)
+table.add_column("Packet Size (bytes)", style="green", justify="center")
+table.add_column("Latency (ms)", style="purple", justify="right", width=13)
+
+serverPort = 5002
+serverIP = '10.245.30.55'
+bufferSize = 10000
+
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server.bind((serverIP, serverPort))
+server.settimeout(5)
+
+console.print("[bold yellow]PC server is up and listening...[/bold yellow]")
+
+packet_id = 1
+
+while True:
+    try:
+        data, address = server.recvfrom(bufferSize)
+        server_received_at = datetime.now()
+
+        #loads data from json 
+        message = json.loads(data.decode('utf-8'))
+
+        #Calc the packet size
+        payload_size = len(data)
+        total_packet_size = payload_size + 28
+
+        #get the timestamp sent from the client 
+        client_sent = datetime.fromisoformat(message['sent_at'])
+        latency_ms = (server_received_at - client_sent).total_seconds() * 1000
+
+        #add rows to the table
+        table.add_row(
+            str(packet_id),
+            message["content"],
+            message["sent_at"],
+            server_received_at.strftime("%Y-%m-%d %H:%M:%S.%f")[:-3],
+            f"{total_packet_size}",
+            f"{latency_ms:.2f}"
+        )
+
+        console.clear()
+        console.print(table)
+
+        packet_id += 1
+
+    except socket.timeout:
+        console.print("[red]No incoming packets. Closing...[/red]")
+        break
+    except KeyboardInterrupt:
+        console.print("[red]Interrupted. Closing...[/red]")
+        break
+
+server.close()
